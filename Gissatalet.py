0ac291f37a7e4059f927b9ebc0303bb47f8e3c54
edu-polmar <polina.martynenko@skola.taby.se>
2025-11-06 12:40:46 +0100

    Update Gissatalet.py
    Del 2: Slumpat tal och gissning

--- Gissatalet.py
+++ Gissatalet.py
@@ -1,2 +1,10 @@
 namn = input("vad heter du? ")
 print(f"Hej {namn} och välkommen till spelet!")
+
+import random
+hemligt_tal = random.randint(1, 10)
+gissning = int(input("Gissa ett tal mellan 1 och 10: "))
+if gissning == hemligt_tal:
+    print("Grattis! Du gissade rätt.")
+else:
+    print(f"Tyvärr, det rätta talet var {hemligt_tal}.")
