c0ee96ac5cdaf2274e032395bac8111f4c125954
edu-polmar <polina.martynenko@skola.taby.se>
2025-11-10 14:52:34 +0100

    Update Gissatalet.py

--- Gissatalet.py
+++ Gissatalet.py
@@ -4,7 +4,13 @@ print(f"Hej {namn} och välkommen till spelet!")
 import random
 hemligt_tal = random.randint(1, 10)
 gissning = int(input("Gissa ett tal mellan 1 och 10: "))
-if gissning == hemligt_tal:
-    print("Grattis! Du gissade rätt.")
-else:
-    print(f"Tyvärr, det rätta talet var {hemligt_tal}.")
+antal_gissningar = 1
+
+while gissning < 4:
+    if gissning != hemligt_tal:
+        print("Fel gissning, försök igen.")
+        gissning = int(input("Gissa ett tal mellan 1 och 10: "))
+        antal_gissningar += 1
+    elif gissning == hemligt_tal:
+        print("Rätt gissat!")
+        break
